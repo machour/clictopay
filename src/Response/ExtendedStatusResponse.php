d463b26dc551a81638ea6aecfaee7ad1e6b9bd9e
Mehdi Achour <machour@gmail.com>
2021-05-31 12:56:11 +0100

    Fix annotations

--- src/Response/ExtendedStatusResponse.php
+++ src/Response/ExtendedStatusResponse.php
@@ -5,18 +5,29 @@ declare(strict_types=1);
 namespace Machour\ClicToPay\Response;
 
 /**
- * @property-read string $expiration
- * @property-read string $cardholderName
- * @property-read integer $depositAmount
+ * @property-read string $orderNumber
+ * @property-read int $orderStatus
+ * @property-read int $actionCode
+ * @property-read string $actionCodeDescription
+ * @property-read string $originalActionCode
+ * @property-read int $amount
  * @property-read string $currency
- * @property-read string $approvalCode
- * @property-read integer $authCode
- * @property-read string $clientId
- * @property-read string $bindingId
- * @property-read string $OrderStatus
- * @property-read string $OrderNumber
- * @property-read string $Pan
- * @property-read integer $amount
+ * @property-read int $date
+ * @property-read int $depositedDate
+ * @property-read string $orderDescription
+ * @property-read string $ip
+ * @property-read array $merchantOrderParams
+ * @property-read array $transactionAttributes
+ * @property-read array $attributes
+ * @property-read array $cardAuthInfo
+ * @property-read int $authDateTime
+ * @property-read string $terminalId
+ * @property-read string $authRefNum
+ * @property-read array $paymentAmountInfo
+ * @property-read array $bankInfo
+ * @property-read array $payerData
+ * @property-read bool $chageback
+ * @property-read string $paymentWay
  */
 class ExtendedStatusResponse extends Response
 {
